9fe5db593de2d0b47d79e8c5e366f0a0519ddddb
rmarticedeno <rmarticedeno@gmail.com>
2018-10-30 11:50:12 -0400

    arreglar error de método split

--- main.c
+++ main.c
@@ -10,33 +10,10 @@ char* user; //nombre de usuario
 
 char* wd; //directorio actual de trabajo
 
-typedef struct cmd {
-	char* id;
-	int args;
-	char** argv;
-} command;
-
 char** split(char *, const char *); //divide un string por un delimitador (repetido n veces n>=1)
-command* split_commands(char*);
-
-void vprint(char** s) {
-	while(*s) {
-		printf("%s ", *s);
-		++s;
-	}
-	printf("\n");
-}
-
-void cprint(command* cmd) {
-	while(cmd->id) {
-		printf("%d ", cmd->args);
-		vprint(cmd->argv);
-		++cmd;
-	}
-	printf("Fin de los comandos\n");
-}
-
-int execute(command, int); //dada una linea parseada ejecuta el comando correspondiente
+void fforf(int, char**); //ejecuta execvp
+void execute(int, char**); //dada una linea parseada ejecuta el comando correspondiente
+void ffree(char**); //libera la memoria de char**
 void init(); //inicializa todos los buffers
 
 int main(int args, const char** argv)
@@ -47,18 +24,11 @@ int main(int args, const char** argv)
         getwd(wd); // obtener el directorio actual
         printf(format, user, hostname, wd); // format definido en .config.h
         getline(&line,&getln_a,stdin);
-        command* cmds = split_commands(line);
-    	cprint(cmds);
-    	int fd = 0;
-    	while(cmds->id) {
-    		fd = execute(*cmds, fd);
-    		++cmds;
-    	}
-    	char c;
-    	while(read(fd, &c, 1)) { printf("%c", c); }
-    	close(fd);
+        char** test = split(line, " ");
+        execute(size, test);
+        ffree(test);
+        free(line);
     }
-	free(line);
 }
 
 void init()
@@ -70,7 +40,7 @@ void init()
     gethostname(hostname,ht_buffer);
 }
 
-char** split(char* string, const char* pattr) // cuando hay espacios al final, size es uno mas de lo normal
+char** split(char* string, const char* pattr)
 {
     //inicializacines
     char** splitted = calloc(10 *sizeof(char*),sizeof(char*));
@@ -119,35 +89,27 @@ char** split(char* string, const char* pattr) // cuando hay espacios al final, s
     return splitted;
 }
 
-command* split_commands(char* string) {
-	char** cmds = split(string, "|");
-	int total = size, i;
-	command* commands = malloc((total + 1)* sizeof(command));
-	
-	for(i = 0; i < total; ++i) {
-		commands[i].argv = split(cmds[i], " ");
-		commands[i].args = size;
-		commands[i].id = commands[i].argv[0];
-	}
-	commands[i].id = 0;
-	return commands;
+void ffree(char** sub) //vaciar un buffer
+{
+    for(int i = 0; i < size; ++i)
+    {
+        free(sub[i]);
+    }
+    free(sub);
 }
 
-int execute(command cmd, int input_fd) //ejecutar un comando
+void execute(int args, char** argv) //ejecutar un comando
 {
-	int p[2];
-	pipe(p);
-    if(!strcmp(cmd.argv[0], "cd")) //si es cd
+    if(!strcmp(argv[0], "cd")) //si es cd
     {
-	close(p[1]);
-        if(cmd.args != 2) // si existen mas de 2 argumentos imprimir error
+        if(args != 2) // si existen mas de 2 argumentos imprimir error
         {
             printf("bash: cd: too many arguments\n");
-            return - 1;
+            return;
         }
         else
         {
-            if(chdir(cmd.argv[1])) // si el chdir retorna error
+            if(chdir(argv[1])) // si el chdir retorna error
             {
                 perror("cd"); //magia :° imprime el mensaje del error con el encabezado que se le pase(cualquier error) :-!
             }
@@ -157,10 +119,8 @@ int execute(command cmd, int input_fd) //ejecutar un comando
 //             }
         }
     }
-    else if(!strcmp(cmd.argv[0], "exit")) //si se escribe el comando exit salir de la consola
-    {	
-    	close(p[0]);
-    	close(p[1]);
+    else if(!strcmp(argv[0], "exit")) //si se escribe el comando exit salir de la consola
+    {
         free(line);
         free(wd);
         free(hostname);
@@ -168,41 +128,38 @@ int execute(command cmd, int input_fd) //ejecutar un comando
     }
     else
     {
-        int pid = fork();
-	    if (!pid)
-	    {
-    		dup2(input_fd, 0);
-    		close(p[0]);
-    		dup2(p[1], 1);
-    		close(p[1]);
-    		// primero sin considerar >, <, >>
-    		char** par = malloc((cmd.args) * sizeof(char*));
-    		int i;
-    		for(i = 0; i <= cmd.args; ++i)
-    		{
-    		    if (i != cmd.args)
-    		    {
-    		        par[i] = cmd.argv[i];
-    		    }
-    		    else
-    		    {
-    		        par[i] = NULL;
-    		    }
-    		}
-    		execvp(cmd.argv[0],par);
-    		exit(EXIT_FAILURE);
-	    }
-	    else
-	    {
-    		int status;
-    		wait(&status);
-    		close(p[1]);
-    		if(!WIFEXITED(status))
-    		{
-    		    perror(cmd.argv[0]); // imprime el error del comando correspondiente
-    		}
-	    }
+        ffork(args, argv); //ejecutar el comando correspondiente con execvp
     }
-	return p[0];
+}
+
+void ffork(int args, char** argv)
+{
+   int pid = fork();
+    if (!pid)
+    {
+        char** par = malloc((args) * sizeof(char));
+        int i;
+        for(i = 0; i <= args; ++i)
+        {
+            if (i != args)
+            {
+                par[i] = argv[i];
+            }
+            else
+            {
+                par[i] = NULL;
+            }
+        }
+        execvp(argv[0],par);
+    }
+    else
+    {
+        int status;
+        wait(&status);
+        if(!WIFEXITED(status))
+        {
+            perror(argv[0]); // imprime el error del comando correspondiente
+        }
+    }
 }
 
